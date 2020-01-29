33ed8ea5dcb91ba0861a6b749d3ef2f4cfe3eb18
Lukáš Kratochvíl <kratochvil.lukas9@gmail.com>
2020-01-29 10:27:42 +0100

    force build

--- packages/client/server.js
+++ packages/client/server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/ping', (req, res) => res.send('pong'));
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
