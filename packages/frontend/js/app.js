908e74eedfdbded23661767abd1f00e1789e561a
Lukáš Kratochvíl <kratochvil.lukas9@gmail.com>
2020-01-29 23:02:14 +0100

    forward to internal port

--- packages/frontend/js/app.js
+++ packages/frontend/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-    var socket = io('ws://be.backend-mmo.app.localspace:8887');
+    var socket = io('wss://be.backend-mmo.app.localspace:8887');
     // var socket = io('http://localhost:8888');
     // logging
     console.log({ socket });
